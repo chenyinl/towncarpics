c9e1be4d557019a7a472b980209bdca27661c5e1
clin <clin@clinA10.(none)>
2014-01-25 22:11:22 -0800

    code clean up

--- function.js
+++ function.js
@@ -1,14 +1,22 @@
+/*
+ * 2014/1/25
+ */
+
+// global variable list of image information
 var imageList=new Array();
 
 $(
 function(){
-
-	Parse.initialize("5vA1TW6f7Ecr1yadxNU0eWKuBXbgZxeyLO6asFCW", "zh6k5tP7JJJ5RbsBlxFVdlMA6l43oGyCOvyJSwtW");
+	// initial Parse key. Read only
+	Parse.initialize(
+	    "5vA1TW6f7Ecr1yadxNU0eWKuBXbgZxeyLO6asFCW", 
+	    "zh6k5tP7JJJ5RbsBlxFVdlMA6l43oGyCOvyJSwtW"
+	);
 	var images = Parse.Object.extend("images");
 	var query = new Parse.Query(images);
 	query.find({
 	    success: function(results) {
-		// Do something with the returned Parse.Object values
+		    // generate the photo info array
 		    for (var i = 0; i < results.length; i++) { 
 		        var object = results[i];
 		        var obj= new Object();
@@ -25,25 +33,16 @@ function(){
      });
 });
 
+/* show the list of photos in the div */
 function showList(){
     $("#selection").html("<ul id='picList'></ul>");
     for (var cnt = 0; cnt < imageList.length; cnt++) {
         $("#picList").append("<li><span onclick='showPic("+cnt+")'>"+imageList[cnt].title +"</span></li>");
     }    
 }
+
+/* show the photos */
 function showPic(n){
 	$("#picture_div").html("<p>"+imageList[n].description+"</p><img src='"+imageList[n].url+"'>");
 }
-/*
 
-function(){
-    console.log(dataJson);
-    $("#selection").append("<ul id='picList'></ul>");
-    for (var cnt = 0; cnt < dataJson.length; cnt++) {
-          $("#picList").append("<li><span onclick='showPic("+cnt+")'>"+dataJson[cnt].title +"</span></li>");
-    }
-
-}
-
-
-*/
