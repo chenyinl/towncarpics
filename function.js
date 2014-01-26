8be3a700e2997429a8944c3cd74c1de844b0f1e1
clin <clin@clinA10.(none)>
2014-01-25 22:24:18 -0800

    change tab to 4 spaces

--- function.js
+++ function.js
@@ -7,29 +7,29 @@ var imageList=new Array();
 
 $(
 function(){
-	// initial Parse key. Read only
-	Parse.initialize(
-	    "5vA1TW6f7Ecr1yadxNU0eWKuBXbgZxeyLO6asFCW", 
-	    "zh6k5tP7JJJ5RbsBlxFVdlMA6l43oGyCOvyJSwtW"
-	);
-	var images = Parse.Object.extend("images");
-	var query = new Parse.Query(images);
-	query.find({
-	    success: function(results) {
-		    // generate the photo info array
-		    for (var i = 0; i < results.length; i++) { 
-		        var object = results[i];
-		        var obj= new Object();
-		        obj.title= object.get('title');
-		        obj.description= object.get('description');
-		        obj.url= object.get('imgFile').url();
-		        imageList.push(obj);
-		    }
-		    showList();
-		},
-		error: function(error) {
-		    alert("Error: " + error.code + " " + error.message);
-		}
+    // initial Parse key. Read only
+    Parse.initialize(
+        "5vA1TW6f7Ecr1yadxNU0eWKuBXbgZxeyLO6asFCW", 
+        "zh6k5tP7JJJ5RbsBlxFVdlMA6l43oGyCOvyJSwtW"
+    );
+    var images = Parse.Object.extend("images");
+    var query = new Parse.Query(images);
+    query.find({
+        success: function(results) {
+            // generate the photo info array
+            for (var i = 0; i < results.length; i++) { 
+                var object = results[i];
+                var obj= new Object();
+                obj.title= object.get('title');
+                obj.description= object.get('description');
+                obj.url= object.get('imgFile').url();
+                imageList.push(obj);
+            }
+            showList();
+        },
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
      });
 });
 
@@ -43,6 +43,6 @@ function showList(){
 
 /* show the photos */
 function showPic(n){
-	$("#picture_div").html("<p>"+imageList[n].description+"</p><img src='"+imageList[n].url+"'>");
+    $("#picture_div").html("<p>"+imageList[n].description+"</p><img src='"+imageList[n].url+"'>");
 }
 
